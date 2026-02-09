6dade6eb77a7e072677888e16f172ffa908c8a60
LilleAila <olai.solsvik@gmail.com>
2026-02-09 15:25:13 +0100

    attempt to use sd

--- tof-prosjekt-26.ino
+++ tof-prosjekt-26.ino
@@ -1,9 +1,28 @@
+#include <SPI.h>
+#include <SdFat.h>
+
+#define SD_CS_PIN A3
+
+SdFat sd;
+File file;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while (!Serial);
 
+  Serial.print("Initializing SD... ");
+
+  if (!sd.begin(SD_CS_PIN, SD_SCK_MHZ(12))) {
+    Serial.println("FAILED");
+    sd.initErrorPrint(&Serial);
+    while (1);
+  }
+
+  Serial.println("SUCCESS");
+
+  file.open("test.txt", O_WRITE | O_CREAT | O_APPEND);
+  file.println("Hello from SdFat!");
+  file.close();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
