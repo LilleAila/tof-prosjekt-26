3df7ae2de2824c982fb508fb7c419d982436fa1c
LilleAila <olai.solsvik@gmail.com>
2026-02-12 08:46:38 +0100

    use sdfat lib

--- tof-prosjekt-26.ino
+++ tof-prosjekt-26.ino
@@ -1,25 +1,28 @@
 #include <SPI.h>
-#include <SD.h>
+#include <SdFat.h>
 
-const int sdPin = 4;
+#define SD_CS_PIN 4
+
+SdFat sd;
+File file;
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
 
-  Serial.print("Initializing SD...");
-  if (!SD.begin(sdPin)) {
-    Serial.println("SD card init failed");
+  Serial.print("Initializing SD... ");
+
+  if (!sd.begin(SD_CS_PIN, SD_SCK_MHZ(12))) {
+    Serial.println("Failed to initialize SD");
+    sd.initErrorPrint(&Serial);
     while (1);
   }
-  Serial.println("SD initialized");
 
-  File dataFile = SD.open("test.txt", FILE_WRITE);
-  if (dataFile) {
-    dataFile.println("Hello, world!");
-    dataFile.flush();
-    dataFile.close();
-  }
+  Serial.println("Initialized SD");
+
+  file.open("test2.txt", O_WRITE | O_CREAT | O_APPEND);
+  file.println("Hello from SdFat!");
+  file.close();
 }
 
 void loop() {}
