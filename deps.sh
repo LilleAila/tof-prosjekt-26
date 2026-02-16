877649b4f3584e815070ed38084f868f26217be2
IldenH <IldenH.1@proton.me>
2026-02-16 13:43:55 +0100

    update deps.sh

--- deps.sh
+++ deps.sh
@@ -4,5 +4,6 @@ arduino-cli core update-index
 arduino-cli core install arduino:samd
 
 arduino-cli lib update-index
+arduino-cli lib install "SdFat"
+arduino-cli lib install "RTCZero"
 arduino-cli lib install "Adafruit BME280 Library"
-
